b0b82875fed869f8064385c19e6ea8deabb4dafc
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-20 18:44:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,12 @@ class Song
   
   def self.artist_count
     artist_count = {}
+      @@artists.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] += 1 
+        else
+          artist_count[ar] = 1 
+        end
     
   end
   
